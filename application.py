cf92f2a74c559ab643801d4a135b220363704fe4
Robert le Clus <rleclus@gmail.com>
2025-06-15 18:34:19 +0200

    Adding some tests to help prepare for pcap exam

--- application.py
+++ application.py
@@ -8,3 +8,50 @@ def halve_strings(string_list):
 	return [halve_string(x) for x in string_list]
 
 print(halve_strings(['Mark', 'Lydia']))
+
+
+class A:
+	x = 'x'
+
+	def alarm(self):
+		print('a' + self.x)
+
+
+class B(A):
+	def alarm(self):
+		super().alarm()
+
+
+class C(B):
+	x = 'y'
+
+
+C().alarm()
+
+file = open('data.txt', 'w+')
+print('Name of the file: ', file.name)
+
+s = 'Peter Wellert\nHello everybody'
+file.write(s)
+file.seek(0)
+for line in file:
+	print(line)
+
+file.close()
+L = [i for i in range(-1, -2)]
+print(len(L))
+
+
+class Class:
+	var = 1
+
+	def __init__(self, value):
+		self.prop = value
+
+
+object_1 = Class(2)
+print(object_1.__dict__)
+x = [0, 1, 2]
+x.insert(0, 1)
+del x[1]
+print(sum(x))
